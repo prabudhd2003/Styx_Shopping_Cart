e697300b4f92d8e223800a9ffcd03f97aa57aee9
Prabudhd Krishna Kandpal <96969241+prabudhd2003@users.noreply.github.com>
2024-07-21 12:54:37 +0530

    Update ProductList.js

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -7,7 +7,7 @@ const ProductList = ({ products, addToCart }) => {
         <div key={product.id} className="product">
           <img src={product.image} alt={product.name} className="product-image" />
           <h3>{product.name}</h3>
-          <p>Price: Rs {product.price}</p>
+          <p>Price: Rs {product.price.replace('$', '')}</p> {/* Ensure no $ symbol */}
           <button onClick={() => addToCart(product)}>Add to Cart</button>
         </div>
       ))}
